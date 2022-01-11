8623986ff7b6126ec2b71c890f18b30991d12cba
Jared Johnson <jjohnson@taxhawk.com>
2022-01-11 15:44:20 -0700

    Kyria keymap 1.0

---
+++ keyboards/splitkb/kyria/keymaps/aranii/config.h
@@ -0,0 +1,50 @@
+/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_HUE_STEP  8
+#    define RGBLIGHT_SAT_STEP  8
+#    define RGBLIGHT_VAL_STEP  8
+#    define RGBLIGHT_LIMIT_VAL 150
+#endif
+
+// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
+// #define SPLIT_USB_DETECT
+#define NO_USB_STARTUP_CHECK
+
+#define LAYER_STATE_8BIT // 8 layers max, to save on space
+
+#undef DRIVER_LED_TOTAL
+#define DRIVER_LED_TOTAL 16
+#undef RGB_MATRIX_SPLIT
+#define RGB_MATRIX_SPLIT { 8, 8 }
+
+#define TAPPING_TOGGLE 1
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 350
+
+#define ADAPTIVE_KEY_TERM 350
+
+#define ONESHOT_TIMEOUT 4000
+#define ONESHOT_TAP_TOGGLE 2
+
+#define COMBO_TERM 45
+#define COMBO_STRICT_TIMER
+#define COMBO_ONLY_FROM_LAYER 0
---
+++ keyboards/splitkb/kyria/keymaps/aranii/macros.c
@@ -0,0 +1,107 @@
+// Macros
+enum custom_keycodes {
+	COPY = SAFE_RANGE,
+	PASTE,
+	CUT,
+	UNDO,
+	REDO,
+    MEH_N,
+    MEH_A,
+    MEH_I,
+    MEH_O,
+    MEH_SPC,
+    MEH_M,
+    MEH_COMM,
+    MEH_U,
+    MEH_Q,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case COPY:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LGUI("c"));
+        } else {}
+        break;
+    case PASTE:
+        if (record->event.pressed) {
+        	register_code(KC_LGUI);
+        	register_code(KC_V);
+        } else {
+        	unregister_code(KC_V);
+        	unregister_code(KC_LGUI);
+        }
+        break;
+    case CUT:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LGUI("x"));
+        } else {}
+        break;
+    case UNDO:
+        if (record->event.pressed) {
+        	register_code(KC_LGUI);
+        	register_code(KC_Z);
+        } else {
+        	unregister_code(KC_Z);
+        	unregister_code(KC_LGUI);
+        }
+        break;
+    case REDO:
+        if (record->event.pressed) {
+        	register_code(KC_LGUI);
+        	register_code(KC_LSFT);
+        	register_code(KC_Z);
+        } else {
+        	unregister_code(KC_Z);
+        	unregister_code(KC_LSFT);
+        	unregister_code(KC_LGUI);
+        }
+        break;
+    case MEH_N:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT("n"))));
+        } else {}
+        break;
+    case MEH_A:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT("a"))));
+        } else {}
+        break;
+    case MEH_I:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT("i"))));
+        } else {}
+        break;
+    case MEH_O:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT("o"))));
+        } else {}
+        break;
+    case MEH_SPC:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT(SS_TAP(X_SPC)))));
+        } else {}
+        break;
+    case MEH_M:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT("m"))));
+        } else {}
+        break;
+    case MEH_COMM:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT(SS_TAP(X_COMM)))));
+        } else {}
+        break;
+    case MEH_U:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT("u"))));
+        } else {}
+        break;
+    case MEH_Q:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT(SS_LCTRL(SS_LSFT("q"))));
+        } else {}
+        break;
+    }
+    return true;
+};
